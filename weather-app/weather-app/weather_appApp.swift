61e648f1ffde12d11d4e1c822f012f14fc8e9c4f
Akshat chaturvedi <60667897+akshat3358@users.noreply.github.com>
2025-04-09 08:56:15 +0530

    Initial Commit

---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,15 @@
+//
+//  weather_appApp.swift
+//  weather-app
+
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
